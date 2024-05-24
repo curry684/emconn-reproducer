62256c72ec7157b2417a66b99fe0c7d29f745402
Niels Keurentjes <niels.keurentjes@omines.com>
2024-05-24 10:47:31 +0200

    Final reproducer version

--- src/Task/CrashHandler.php
+++ src/Task/CrashHandler.php
@@ -3,20 +3,25 @@
 namespace App\Task;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\Exception\RecoverableMessageHandlingException;
 
 #[AsMessageHandler]
 readonly class CrashHandler
 {
-    public function __construct(private EntityManagerInterface $em)
+    public function __construct(private EntityManagerInterface $em, private LoggerInterface $logger)
     {
     }
 
     public function __invoke(CrashMessage $message): void
     {
+        $this->logger->info('Closing entity manager');
+
         $this->em->close();
 
-        throw new RecoverableMessageHandlingException('Moar!');
+        $this->logger->info('Throwing unrecoverable exception');
+
+        throw new \RuntimeException('Moar!');
     }
 }
